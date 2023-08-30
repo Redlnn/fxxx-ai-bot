71b313730faa0b0d83b1ff8940435bbf09bfa060
Kaedehara Kazuha <w731347477@gmail.com>
2023-08-30 18:59:14 +0800

    视频评论区关键词屏蔽

--- userscript.js
+++ userscript.js
@@ -5,7 +5,7 @@
 // @match       *://t.bilibili.com/*
 // @match       *://space.bilibili.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Red_lnn
 // @description 滚啊！总结Bot
 // @namespace   https://github.com/Redlnn/fxxx-ai-bot
@@ -17,6 +17,7 @@
 
 const banList = {
   summary: ['1141159409', '8455326', '234978716', '3494380876859618', '1168527940', '1358327273'],
+  keyword: ['AI视频小助理', '机器工具人', '有趣的程序员', '课代表猫', 'AI课代表呀', '星崽丨StarZai'],
   list: [
     {
       name: 'AI视频小助理',
@@ -57,10 +58,30 @@ const watchingCommentDomList = []
 // -----------------------
 
 async function modReply(listDom, replyDom) {
-  if (!replyDom.classList.contains('reply-item')) return
+  if (!replyDom.classList || !replyDom.classList.contains('reply-item')) return
 
   const rootReply = replyDom.querySelector('.root-reply-container')
   const subReplyList = replyDom.querySelectorAll('.sub-reply-item')
+
+  // 查找主楼内容是否有关键词
+  const rootContent = rootReply.querySelector('.reply-content')
+  banList.keyword.forEach(async (keyword) => {
+    if (rootContent.textContent.includes(keyword)) {
+      if (subReplyList.length === 0) {
+        replyDom.style.background = '#FFB7B7'
+      } else {
+        rootReply.style.background = '#FFB7B7'
+      }
+
+      replyDom.style.transition = 'all 0.3s'
+      replyDom.style['-webkit-transition'] = 'all 0.3s'
+      replyDom.style.heiht = '0'
+      replyDom.style.display = 'none'
+
+      console.warn('发现提到总结Bot的评论')
+      console.log(replyDom)
+    }
+  })
 
   // 判断主楼发表用户
   const userName = replyDom.querySelector('.root-reply-container .root-reply-avatar')
@@ -75,6 +96,9 @@ async function modReply(listDom, replyDom) {
     replyDom.style['-webkit-transition'] = 'all 0.3s'
     replyDom.style.heiht = '0'
     replyDom.style.display = 'none'
+
+    console.warn('发现AI总结Bot的评论')
+    console.log(replyDom)
   }
 
   // 判断主楼首层被@用户
@@ -92,12 +116,31 @@ async function modReply(listDom, replyDom) {
         replyDom.style['-webkit-transition'] = 'all 0.3s'
         replyDom.style.heiht = '0'
         replyDom.style.display = 'none'
+
+        console.warn('发现@AI总结Bot')
+        console.log(replyDom)
       }
     })
   }
 
   if (subReplyList.length > 0) {
     subReplyList.forEach(async (subReplyDom) => {
+      // 查找子评论内容是否有关键词
+      const subReplyContent = subReplyDom.querySelector('.reply-content')
+      banList.keyword.forEach(async (keyword) => {
+        if (subReplyContent.textContent.includes(keyword)) {
+          subReplyDom.style.background = '#FFB7B7'
+
+          subReplyDom.style.transition = 'all 0.3s'
+          subReplyDom.style['-webkit-transition'] = 'all 0.3s'
+          subReplyDom.style.heiht = '0'
+          subReplyDom.style.display = 'none'
+
+          console.warn('发现提到总结Bot的子评论')
+          console.log(replyDom)
+        }
+      })
+
       // 判断子楼发表用户
       const userName = subReplyDom.querySelector('.sub-reply-container .sub-reply-avatar')
       if (banList.summary.includes(userName.getAttribute('data-user-id'))) {
@@ -107,6 +150,9 @@ async function modReply(listDom, replyDom) {
         subReplyDom.style['-webkit-transition'] = 'all 0.3s'
         subReplyDom.style.heiht = '0'
         subReplyDom.style.display = 'none'
+
+        console.warn('发现子评论有AI总结Bot')
+        console.log(replyDom)
       }
 
       // 判断子楼首层被@用户
@@ -120,6 +166,9 @@ async function modReply(listDom, replyDom) {
             subReplyDom.style['-webkit-transition'] = 'all 0.3s'
             subReplyDom.style.heiht = '0'
             subReplyDom.style.display = 'none'
+
+            console.warn('发现子评论@AI总结Bot')
+            console.log(replyDom)
           }
         })
       }
@@ -157,6 +206,9 @@ async function modComment(listDom, commentDom) {
     } else {
       commentDom.querySelector('.con > .text').style.background = '#FFB7B7'
     }
+
+    console.warn('发现AI总结Bot的评论')
+    console.log(replyDom)
   }
 
   // 判断主楼首层被@用户
@@ -172,6 +224,9 @@ async function modComment(listDom, commentDom) {
           commentDom.querySelector('.con > .text').style.background = '#FFB7B7'
         }
       }
+
+      console.warn('发现@AI总结Bot')
+      console.log(replyDom)
     })
   }
 
@@ -181,6 +236,9 @@ async function modComment(listDom, commentDom) {
       const userName = subCommentDom.querySelector('.reply-face')
       if (banList.summary.includes(userName.getAttribute('data-usercard-mid'))) {
         subCommentDom.style.background = '#FFB7B7'
+
+        console.warn('发现子评论有AI总结Bot')
+        console.log(replyDom)
       }
 
       // 判断子楼首层被@用户
@@ -191,6 +249,9 @@ async function modComment(listDom, commentDom) {
           if (linkAttr === null) return
           if (banList.summary.includes(linkAttr)) {
             subCommentDom.style.background = '#FFB7B7'
+
+            console.warn('发现子评论@AI总结Bot')
+            console.log(replyDom)
           }
         })
       }
@@ -225,7 +286,7 @@ async function modComment(listDom, commentDom) {
           if (!watchingReplyDomList.includes(replayListDom)) {
             watchingReplyDomList.push(replayListDom)
             replayListDom.childNodes.forEach(async (node) => {
-              if (node.classList.contains('reply-item')) {
+              if (node.classList && node.classList.contains('reply-item')) {
                 await modReply(replayListDom, node)
               }
             })
@@ -244,7 +305,7 @@ async function modComment(listDom, commentDom) {
         commentListDoms.forEach(async (commentListDom) => {
           watchingCommentDomList.push(commentListDom)
           commentListDom.childNodes.forEach(async (node) => {
-            if (node.classList.contains('list-item')) {
+            if (node.classList && node.classList.contains('list-item')) {
               await modComment(commentListDom, node)
             }
           })
